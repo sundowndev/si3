a12cd13d314c56dfe65ec7bc31de2b76d07be824
sundowndev <anonhq789@protonmail.com>
2017-12-18 10:27:19 +0100

    init project

---
+++ assets/js/data.js
@@ -0,0 +1,327 @@
+var data = {
+    categories: ['Action', 'Animation', 'Comedy', 'Horror / Thriller'],
+    max_rating: 5,
+    films: [{
+        src: 'TIE+Fighter+-+short+film(720p).mp4',
+        title: 'TIE Fighter - short film',
+        duration: '7:27',
+        author: 'OtaKing77077',
+        author_url: 'https://www.youtube.com/channel/UCRBNWKYrybXl2Un6-ejwx8g',
+        description: {
+          fr: '???',
+          en: 'hat if there was an Empire-focussed short Star Wars animation, drawn with the crazy detail and shading of classic 80s anime that\'s all but vanished from Japan nowadays? <br /> Well, I tried my best. Apologies in advance for not living up to Venus Wars standards. <br /> Drawn and animated by yours truly over 4 years\' worth of weekends, with music by the living guitar solo Zak Rahman and sound design by up and coming audio technician Joseph Leyva. Fans of Lucasarts\' seminal 1994 TIE Fighter game may notice a few familiar sights and sounds. That “incoming missile” noise gives me horrible flashbacks to this day...',
+          ch: '???'
+        },
+        year: 2015,
+        audio_language: 'N/A',
+        sub_language: 'N/A',
+        rating: 4,
+        category: 'Animation'
+    }, {
+        src: 'Naruto+Live+Action+(avec+Florian+Nguyen+et+Aziatomik)(720p).mp4',
+        title: 'Naruto Live Action (avec Florian Nguyen et Aziatomik)',
+        duration: '1:47',
+        author: 'Brice Duan',
+        author_url: 'https://www.youtube.com/channel/UCYF-3u_Jh5lWzPG2gRDD3-Q',
+        description: {
+          fr: 'Naruto Live Action - "L\'Héritage". Un membre de l\'Akatsuki est venu défier les héritiers de Naruto ! Il s\'agit d\'un fan film rendant hommage à l\'oeuvre de Masashi Kishimito. Merci à Rivenx3i pour m\'avoir inspiré avec sa vidéo Naruto Shippuden Dreamers Fight, le meilleur fan film sur Naruto à ce jour',
+          en: '???',
+          ch: '???'
+        },
+        year: 2013,
+        audio_language: 'Français',
+        sub_language: 'N/A',
+        rating: 3,
+        category: 'Action'
+    }, {
+        src: 'Dragon+Ball+vs+Naruto.+Anime+fight.(720p).mp4',
+        title: 'Dragon Ball vs Naruto. Anime fight',
+        duration: '8:39',
+        author: 'Alberto Barberá',
+        author_url: 'https://www.youtube.com/channel/UCaH3HAq1O9rJvTUn0mlVVkw',
+        description: {
+          fr: '???',
+          en: '???',
+          ch: '???',
+          es: 'Pelea brutal con un toque de humor y comedia. La idea surgió jugando al Dragon Ball Xenoverse después de leer el manga de Naruto Shippuden mientras me convertía en Super Saiyan y escuchaba el rap de Naruto y Sasuke. Love Dragon Ball games. Waiting for Naruto Gaiden to come soon... Mientras disfrutemos de DB Super.'
+        },
+        year: 2015,
+        audio_language: 'N/A',
+        sub_language: 'N/A',
+        rating: 5,
+        category: 'Action'
+    }, {
+        src: 'Naruto+Shippuden_+Dance+of+War+-+Short+Film+(Turn+On+Subtitles)(1080p).mp4',
+        title: 'Naruto Shippuden: Dance of War - Short Film',
+        duration: '7:48',
+        author: 'RivenX3i',
+        author_url: 'https://www.youtube.com/channel/UC-Op8pOVwbeqp6lyL2xJvqQ',
+        description: {
+          fr: '???',
+          en: 'The 4th Great Ninja War is near. Two Hidden Leaf ninja say goodbye to where they grew into ninja, by holding a ceremony to test their own resolve.',
+          ch: '???',
+          es: '???'
+        },
+        year: 2015,
+        audio_language: 'Japonais',
+        sub_language: 'N/A',
+        rating: 4,
+        category: 'Action'
+    }, {
+        src: 'NARUTO+-+New+Movie+2018+-+Full+Trailer+AMAZING!!(720p).mp4',
+        title: 'NARUTO - New Movie 2018',
+        duration: '12:50',
+        author: 'RivenX3i',
+        author_url: 'https://www.youtube.com/channel/UC-Op8pOVwbeqp6lyL2xJvqQ',
+        description: {
+          fr: '???',
+          en: 'The 4th Great Ninja War is near. Two Hidden Leaf ninja say goodbye to where they grew into ninja, by holding a ceremony to test their own resolve.',
+          ch: '???',
+          es: '???'
+        },
+        year: 2016,
+        audio_language: 'Japonais',
+        sub_language: 'N/A',
+        rating: 3,
+        category: 'Action'
+    }, {
+        src: 'Naruto+The+Movie!+(Official+Fake+Trailer)(720p).mp4',
+        title: 'Naruto The Movie! (Official Fake Trailer)',
+        duration: '3:15',
+        author: 'nigahiga',
+        author_url: 'https://www.youtube.com/channel/UCSAUGyc_xA8uYzaIVG6MESQ',
+        description: {
+          fr: '???',
+          en: 'Huge THANK YOU to Portal A, Gritty Reboots and everyone on the Japan team for helping out with this! #grubblife',
+          ch: '???',
+          es: '???'
+        },
+        year: 2013,
+        audio_language: 'Anglais',
+        sub_language: 'N/A',
+        rating: 3,
+        category: 'Comedy'
+    }, {
+        src: 'Red+Hood_+The+Fallen+-+DC+Comic+Batman+Fan+Film(720p).mp4',
+        title: 'Red Hood: The Fallen - DC Comic Batman Fan Film',
+        duration: '23:16',
+        author: 'Toby Bajrovic',
+        author_url: 'https://www.youtube.com/channel/UCjGUxyIEtqh_CyWBIVIhzng',
+        description: {
+          fr: '???',
+          en: 'Red Hood: The Fallen is a fan made DC Comic film produced in Perth, Western Australia.',
+          ch: '???',
+          es: '???'
+        },
+        year: 2015,
+        audio_language: 'Anglais',
+        sub_language: 'N/A',
+        rating: 5,
+        category: 'Action'
+    }, {
+        src: 'JUSTICE+LEAGUE_+Trinity+Force+_+DC+FAN+FILM+_(720p).mp4',
+        title: 'JUSTICE LEAGUE: Trinity Force | DC FAN FILM |',
+        duration: '16:29',
+        author: 'SHIELDS Productions',
+        author_url: 'https://www.youtube.com/channel/UCaz9yZYvDvbG_mgPhcCn4pw',
+        description: {
+          fr: '???',
+          en: 'Four months after an earth shattering event, Broken, The Justice League must put their differences aside to stop The Joker!',
+          ch: '???',
+          es: '???'
+        },
+        year: 2015,
+        audio_language: 'Anglais',
+        sub_language: 'N/A',
+        rating: 2.5,
+        category: 'Action'
+    }, {
+        src: 'The+Mad+Hatter+-+Fan+Film(720p).mp4',
+        title: 'The Mad Hatter - Fan Film',
+        duration: '6:16',
+        author: 'Wolfspider Productions',
+        author_url: 'https://www.youtube.com/channel/UC_BAlJSUbpqzSjNX8on3Rgw',
+        description: {
+          fr: '???',
+          en: 'A dark modern interpretation on the DC comic villain, Jervis Tetch aka The Mad Hatter. Set in the time of the modern interpretations on Alice in Wonderland.',
+          ch: '???',
+          es: '???'
+        },
+        year: 2016,
+        audio_language: 'N/A',
+        sub_language: 'N/A',
+        rating: 2,
+        category: 'Horror / Thriller'
+    }, {
+        src: 'The+Flash+Lite+(comedy+fan+film)(720p).mp4',
+        title: 'The Flash Lite (comedy fan film)',
+        duration: '10:14',
+        author: 'The Chublin Brothers',
+        author_url: 'https://www.youtube.com/channel/UCPFmTZlrfUkRCjGgSLtqx1g',
+        description: {
+          fr: '???',
+          en: 'A world-class athlete, but otherwise ordinary guy, assumes the identity of The Flash after being named the "Fastest Man Alive."  But will being slightly faster than everyone else, with exactly zero superpowers, give him the crime-fighting edge he needs?',
+          ch: '???',
+          es: '???'
+        },
+        year: 2013,
+        audio_language: 'Anglais',
+        sub_language: 'N/A',
+        rating: 4,
+        category: 'Comedy'
+    }, {
+        src: 'Clash+at+the+Cantina+(2014)+-+Star+Wars+Fan+Film(720p).mp4',
+        title: 'Clash at the Cantina (2014) - Star Wars Fan Film',
+        duration: '4:45',
+        author: 'Corey Vidal',
+        author_url: 'https://www.youtube.com/channel/UCRBNWKYrybXl2Un6-ejwx8g',
+        description: {
+          fr: '???',
+          en: 'As part of the on-going Star Wars-inspired action comedy video series between YouTubers Corey Vidal (ApprenticeA) and Craig Benzine (Wheezy Waiter), the guys take the battle to the Cantina with a new lightsaber fight.',
+          ch: '???',
+          es: '???'
+        },
+        year: 2014,
+        audio_language: 'Anglais',
+        sub_language: 'N/A',
+        rating: 4,
+        category: 'Comedy'
+    }, {
+        src: 'THE+74th+HUNGER+GAMES+(SHORT+COMEDY+FAN+FILM)(720p).mp4',
+        title: 'THE 74th HUNGER GAMES (SHORT COMEDY FAN FILM)',
+        duration: '4:24',
+        author: 'ChickenFeetFilms',
+        author_url: 'https://www.youtube.com/channel/UC9nzWgRq-cQcakkzZq1XZvg',
+        description: {
+          fr: '???',
+          en: 'A short "HUNGER GAMES" fan film, made by ChickenFeetFilms.',
+          ch: '???',
+          es: '???'
+        },
+        year: 2012,
+        audio_language: 'Anglais',
+        sub_language: 'N/A',
+        rating: 2,
+        category: 'Comedy'
+    }, {
+        src: 'VICTIM+-+A+Short+Slender+Man+Horror+Film(720p).mp4',
+        title: 'VICTIM - A Short Slender Man Horror Film',
+        duration: '14:30',
+        author: 'Eddie Adamson',
+        author_url: 'https://www.youtube.com/channel/UCpWweEWZIfalXgKS1xO2RDw',
+        description: {
+          fr: '???',
+          en: 'This is a Short Horror film I made for my last college project in Creative Media Production, before I went off to University. We were given the chance to individually make a film of our own choice, of varied length and subject matter.',
+          ch: '???',
+          es: '???'
+        },
+        year: 2013,
+        audio_language: 'N/A',
+        sub_language: 'N/A',
+        rating: 4,
+        category: 'Horror / Thriller'
+    }, {
+        src: 'DON&#039;T+MOVE+-+A+Short+Horror+Film+(2017)(720p).mp4',
+        title: 'FALLOUT - Apocalyptic Thriller Short Film',
+        duration: '7:27',
+        author: 'AndrewProductionsTV',
+        author_url: 'https://www.youtube.com/channel/UCOAHgaVjyUjFtxhgCAQ_vAg',
+        description: ' ',
+        description: {
+          fr: '???',
+          en: 'FALLOUT -  Apocalyptic Thriller Short Film. Aaron, a young researcher seeks to discover the deadly disease that plagues Earth. This project is meant to convey the philosophical message that Humans are the deadliest beings to ever walk the Earth through Industrialism, Animal Cruelty/Exploitation, and War.',
+          ch: '???',
+          es: '???'
+        },
+        year: 2015,
+        audio_language: 'N/A',
+        sub_language: 'N/A',
+        rating: 3,
+        category: 'Horror / Thriller'
+    }, {
+        src: 'Dragon+Ball+Z_+Light+of+Hope+-+Pilot(720p).mp4',
+        title: 'Dragon Ball Z: Light of Hope - Pilot',
+        duration: '13:14',
+        author: 'RobotUnderdog2',
+        author_url: 'https://www.youtube.com/channel/UCkeLPTcjQm3zlPuDdvS4gvg',
+        description: {
+          fr: '???',
+          en: 'This series is an adaptation of “The History of Trunks” TV Special. In this timeline, Goku & the other Z Warriors are dead. This is the story of Gohan & Trunks in their battle to survive against the Androids.',
+          ch: '???',
+          es: '???'
+        },
+        year: 2015,
+        audio_language: 'Anglais',
+        sub_language: 'N/A',
+        rating: 4,
+        category: 'Action'
+    }, {
+        src: 'Attack+on+Titan+-+LEVI+vs+BEAST+TITAN+-+Fan+made+animation+-+Shingeki+no+kyojin+manga+chapter+81(480p).mp4',
+        title: 'Attack on Titan - LEVI vs BEAST TITAN - Fan made animation',
+        duration: '1:14',
+        author: 'S.L.H Animation',
+        author_url: 'https://www.youtube.com/channel/UCE6CEl3ZoiDLUhawFG5xn1g',
+        description: {
+          fr: '???',
+          en: 'It is a fan made animation of Attack on Titan. It was for fun and I do not own right to Attack in Titan. All credit go to WIT studio, Production I.G, and Hajime Isayama for such amazing series.',
+          ch: '???',
+          es: '???'
+        },
+        year: 2017,
+        audio_language: 'N/A',
+        sub_language: 'N/A',
+        rating: 4,
+        category: 'Animation'
+    }, {
+        src: 'Chinese+teenager+created+awesome+fight+scene+animation(536p).mp4',
+        title: 'Personal Animated Short 【Heat vs Repulsion】自主制作アニメ',
+        duration: '3:50',
+        author: 'magnil',
+        author_url: 'https://www.youtube.com/channel/UCRBNWKYrybXl2Un6-ejwx8g',
+        description: {
+          fr: '???',
+          en: '???',
+          ch: '中国19岁学生制作的个人动画短片《高温vs排斥》，作者：光学核心(Optical-Core)。概念、动画、背景、合成均由作者个人完成。',
+          es: '???'
+        },
+        year: 2015,
+        audio_language: 'N/A',
+        sub_language: 'N/A',
+        rating: 5,
+        category: 'Animation'
+    }, {
+        src: 'SPAWN_+THE+RECALL+-+FAN+FILM(720p).mp4',
+        title: 'SPAWN: THE RECALL - FAN FILM',
+        duration: '7:56',
+        author: 'Irissee',
+        author_url: 'https://www.youtube.com/channel/UCefxt5RuoiBlanGXmh4vg_A',
+        description: {
+          fr: '???',
+          en: 'SPAWN : THE RECALL is the story of a former witch and her son, who try to live a normal life away from the darkness.',
+          ch: '???',
+          es: '???'
+        },
+        year: 2014,
+        audio_language: 'Anglais',
+        sub_language: 'N/A',
+        rating: 4,
+        category: 'Horror / Thriller'
+    }, {
+        src: '[Rebeler]+Madara+VS+Hashirama+-+Cosplay+Movie(720p).mp4',
+        title: '[Rebeler] Madara VS Hashirama - Cosplay Movie',
+        duration: '8:06',
+        author: 'Rebeler Team',
+        author_url: 'https://www.youtube.com/channel/UCSDJAB-afLRrbytxQuq-lAg',
+        description: {
+          fr: '???',
+          en: 'REBELER adalah Organisasi Cosplay yang Independent berbentuk Team di daerah Bojonegoro - Jawa Timur, yang mempunyai tujuan untuk mengembangkan kreativitas para membernya dalam dunia percosplayan.',
+          ch: '???',
+          es: '???'
+        },
+        year: 2016,
+        audio_language: 'N/A',
+        sub_language: 'N/A',
+        rating: 4,
+        category: 'Action'
+    }, ]
+};
---
+++ assets/js/player.js
@@ -0,0 +1,186 @@
+function videoPlayer(player, options) {
+    this.player = document.querySelector(player);
+    this.options = options;
+    
+    /* Adding additional params */
+    this.options.index = 0;
+    this.options.current_timeline = '00:00';
+    this.options.timeline = '00:00';
+    this.options.playing = false;
+    
+    /* Init the player */
+    var videoTag = document.createElement('video');
+    this.player.appendChild(videoTag);
+    videoTag.preload = 'auto';
+    
+    var mainDiv = document.createElement('div');
+    mainDiv.classList.add('mainDiv');
+    this.player.appendChild(mainDiv);
+    
+    /* Play button */
+    var pButton = document.createElement('button');
+    pButton.classList.add('pButton');
+    mainDiv.appendChild(pButton);
+    pButton.innerHTML = 'play';
+    
+    /* Timeline element */
+    var timeline = document.createElement('div');
+    timeline.classList.add('timeline');
+    mainDiv.appendChild(timeline);
+    
+    var progress = document.createElement('div');
+    progress.classList.add('progress');
+    timeline.appendChild(progress);
+    
+    var loaded = document.createElement('div');
+    loaded.classList.add('loaded');
+    timeline.appendChild(loaded);
+    
+    /* Song duration element */
+    var timer = document.createElement('div');
+    timer.classList.add('timer');
+    mainDiv.appendChild(timer);
+    
+    /* Volume control element */
+    var volumeControl = document.createElement('div');
+    volumeControl.classList.add('volumeControl');
+    this.player.appendChild(volumeControl);
+    
+    /* functions */
+    this.setSong = function(id){
+        this.options.index = id;
+        audio.src = this.options.songList[id].path;
+        audio.load();
+    }
+    
+    this.play = function(){
+        audio.play();
+        pButton.innerHTML = 'pause';
+    }
+    
+    this.pause = function(){
+        audio.pause();
+        pButton.innerHTML = 'play';
+    }
+    
+    this.stop = function(){
+        audio.pause();
+        audio.currentTime = 0;
+        pButton.innerHTML = 'play';
+        options.playing = false;
+    }
+    
+    this.prev = function(){
+        if(options.songList.indexOf(options.songList[options.index]) != 0){
+            options.index -= 1;
+            this.setSong(options.index);
+            this.stop();
+            
+            if(options.playing === true){
+                this.play();
+            }else{
+                this.stop();
+            }
+        }else if(options.playing === false){
+            this.stop();
+        }
+    }
+    
+    this.next = function(){
+        if(options.songList.indexOf(options.songList[options.index]) != options.songList.length - 1){
+            options.index += 1;
+            this.setSong(options.index);
+            this.pause();
+            
+            if(options.playing === true){
+                this.play();
+            }else{
+                this.pause();
+            }
+        }else{
+            this.stop();
+        }
+    }
+    
+    this.setVolume = function(vol){
+        audio.volume = vol * 0.01; // round 100 to 1
+    }
+    
+    var getCurrentTimerPourcentage = function(){
+        //audio.currentTime/audio.duration
+        //timeline.style.width = '40%';
+    }
+    
+    var getCurrentTimerSec = function(){
+        //audio.currentTime/audio.duration
+        
+        timeline.style.width = '40%';
+    }
+    
+    var setDuration = function(duration){
+        var minutes = "0" + Math.floor(duration / 60);
+        var seconds = "0" + (Math.floor(duration) - minutes * 60);
+        options.timeline = minutes.substr(-2) + ":" + seconds.substr(-2);
+    }
+    
+    this.setCurrentTimer = function(currentTime){
+        var minutes = "0" + Math.floor(currentTime / 60);
+        var seconds = "0" + (Math.floor(currentTime) - minutes * 60);
+        this.options.current_timeline = minutes.substr(-2) + ":" + seconds.substr(-2);
+    }
+    
+    this.refreshTimer = function(){
+        this.setCurrentTimer(audio.currentTime);
+        
+        timer.innerHTML = '<span class="played">'+this.options.current_timeline+'</span> / <strong class="duration">'+this.options.timeline+'</strong>';
+    }
+    
+    /* Create the audio object */
+    var audio = new Audio();
+    
+    this.setSong(this.options.index);
+    this.setVolume(this.options.defaultVolume);
+    
+    audio.addEventListener('canplay', function() {
+        setDuration(audio.duration);
+        self.refreshTimer();
+    });
+    
+    /* Using params */
+    if(this.options.autoplay === true){
+        this.play();
+    }
+    
+    /* triggering elements */
+    pButton.addEventListener('click', function(){
+        if(options.playing === false){
+            nanoPlayer.play();
+            options.playing = true;
+        }else{
+            nanoPlayer.pause();
+            options.playing = false;
+        }
+    });
+    
+    timeline.addEventListener('click', function(){
+        //mettre à jour progress bar en fonction du clic
+        //maj le timer
+        //console.log('test1');
+    });
+    
+    audio.addEventListener("playing", function(){
+        nanoPlayer.options.playing = true;
+    });
+    
+    audio.addEventListener("timeupdate", function(){
+        nanoPlayer.refreshTimer();
+    });
+    
+    audio.addEventListener("ended", function(){
+        if(options.loop === true && options.songList.indexOf(options.songList[options.index]) == options.songList.length - 1){
+            nanoPlayer.play();
+        }else{
+            nanoPlayer.next();
+        }
+    });
+}
